139b7c2b896973d906e179997fb6eae966d33f3d
Ricardo-M-Sakura <66266662+Ricardo-M-Sakura@users.noreply.github.com>
2023-04-21 20:46:52 +0800

    Add files via upload

---
+++ sum/sum.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int sum(int N);
+
+
+int sum2(int N);
+
+
+int main(int argc, char **argv){
+  if(argc == 1) {
+    printf("usage: %s <int>\n", *argv);
+    return 1;
+  }
+  int N = atoi(argv[1]);
+  int gold = sum2(N);
+  int s = sum(N);
+
+  if(s != gold) {
+    printf("Error: got %d expected %d\n", s, gold);
+    return 2;
+  }
+
+  printf("Success: sum(%d) = %d\n", N, s);
+  return 0;
+}
+
+int sum(int N) {
+  int i;
+  int s = 0;
+  for(i = 1; i <= N; ++i) {
+    s += i;
+  }
+  return s;
+}
+
+int sum2(int N) {
+  return N*(N+1)/2;
+}
